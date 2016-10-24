8d302b6c4ce8771e53560df10d7bec385ee4894e
Draghia Andrei <draghia.andrei_bv@yahoo.ro>
2016-10-24 13:55:48 +0300

    add ShoppingList project

---
+++ ShoppingList/www/cordova.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Andrei on 10/24/2016.
+ */
---
+++ ShoppingList/www/js/app.js
@@ -0,0 +1,85 @@
+(function(){
+  'use strict';
+
+  angular.module("ShoppingList", ['ionic'])
+    .controller("ToBuyController", ToBuyController)
+    .controller("BoughtController", BoughtController)
+    .controller("ErrorController", ErrorController)
+    .service("ShoppingService", ShoppingService);
+
+  ToBuyController.$inject = ["ShoppingService"];
+  function ToBuyController (ShoppingService) {
+    var toBuy = this;
+
+    toBuy.addItemToBuy = function (itemIndex) {
+      ShoppingService.addItemToBuy(itemIndex);
+    };
+
+    toBuy.items = ShoppingService.showToBuyList();
+
+    toBuy.moveItem = function (itemIndex) {
+      ShoppingService.moveItem(itemIndex);
+    };
+  }
+
+  BoughtController.$inject = ["ShoppingService"];
+  function BoughtController (ShoppingService) {
+    var alreadyBought = this;
+
+    alreadyBought.items = ShoppingService.getBoughtItems();
+
+    alreadyBought.removeItem = function (itemIndex) {
+      ShoppingService.removeItem(itemIndex);
+    }
+  }
+
+  ErrorController.$inject = ["ShoppingService"];
+  function ErrorController (ShoppingService) {
+    var errorMessage = this;
+    errorMessage.message = ShoppingService.getMessage();
+  }
+
+  function ShoppingService () {
+    var service = this;
+
+    var toBuyList = [];
+    var boughtList = [];
+    var message = "";
+
+    service.addItemToBuy = function (item) {
+      item = item.replace(/[^A-Z]/ig, '');
+      if(item.length <= 0) {
+        message = "Please insert data first!";
+        return;
+      }
+      if(toBuyList.indexOf(item) == -1) {
+        toBuyList.push(item);
+        message = "";
+      }
+      else {
+        message = "The item is already in your shopping list!";
+      }
+    };
+
+    service.moveItem = function (itemIndex) {
+      boughtList.push(toBuyList[itemIndex]);
+      toBuyList.splice(itemIndex, 1);
+    };
+
+    service.removeItem = function (itemIndex) {
+      boughtList.splice(itemIndex, 1);
+    };
+
+    service.showToBuyList = function () {
+      return toBuyList;
+    };
+
+    service.getBoughtItems = function () {
+      return boughtList;
+    };
+
+    service.getMessage = function () {
+      return message;
+    }
+  }
+})();
